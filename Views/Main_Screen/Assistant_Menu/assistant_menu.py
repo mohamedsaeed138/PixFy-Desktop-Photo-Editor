de9b33a71a882f24a7e52ae22ce6128fff4bcc4b
Mohamed Saeed <91873184+mohamedsaeed138@users.noreply.github.com>
2023-12-03 20:36:43 +0200

    .

--- Views/Main_Screen/Assistant_Menu/assistant_menu.py
+++ Views/Main_Screen/Assistant_Menu/assistant_menu.py
@@ -6,8 +6,9 @@ class AssistantMenu(CTkFrame):
         super().__init__(
             master, fg_color=["#EAEAEA", "white"], width=size[0], height=size[1]
         )
-        self.grid_columnconfigure(list(range(0, 3)), pad=10)
-        self.grid_rowconfigure((0), pad=30)
+        self.grid_columnconfigure(list(range(0, 3)), pad=int(size[0] * 0.015))
+
+        self.grid_rowconfigure((0), pad=int(size[1] * 0.48))
         self.grid_propagate(False)
         self.upload_btn = CTkButton(
             self,
@@ -16,7 +17,7 @@ class AssistantMenu(CTkFrame):
             text="Upload Image",
             corner_radius=10,
         )
-        self.upload_btn.grid(row=0, column=0, padx=15)
+        self.upload_btn.grid(row=0, column=0, padx=int(size[0] * 0.022))
         self.save_btn = CTkButton(
             self,
             width=size[0] // 3,
